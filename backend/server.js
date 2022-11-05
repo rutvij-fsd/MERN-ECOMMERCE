12f2543077d0c9bc3aeb4dd19f8e719866f76db1
rutvij-fsd <rutvijpatel414@gmail.com>
2022-11-05 22:09:56 +0530

    Coonect To Database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const app = require("./app");
+const dotenv = require("dotenv");
 
+const connectDatabase = require("./config/database");
+// config
+dotenv.config({ path: "backend/config/config.env" });
 
-// config 
-dotenv.config({path:"backend/config/config.env"});
+//Connecting to database
+connectDatabase();
 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server listening on port '+process.env.PORT);
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
