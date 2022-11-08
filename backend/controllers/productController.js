0b487214761ac64e01ffd05edbc023337d77f77b
rutvij-fsd <rutvijpatel414@gmail.com>
2022-11-08 20:41:01 +0530

    Add AsyncError Handler

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,28 +1,29 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncError = require("../middleware/catchAsyncError");
 
 // Create Product ---Admin
 
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 //Get all products
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncError(async (req, res) => {
   const product = await Product.find();
 
   res.status(200).json({
     success: true,
     product,
   });
-};
+});
 
 //Get a Product
-exports.getProductDetail = async (req, res, next) => {
+exports.getProductDetail = catchAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -33,10 +34,10 @@ exports.getProductDetail = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 //Update Product ---Admin
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -52,10 +53,10 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 //Delete a product ---Admin
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -65,4 +66,4 @@ exports.deleteProduct = async (req, res, next) => {
   await product.remove();
 
   res.status(200).json({ success: true, message: "Product deleted" });
-};
+});
