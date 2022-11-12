538daed1563b1a6103d22745cd2f49053725dbc6
rutvij-fsd <rutvijpatel414@gmail.com>
2022-11-12 16:24:00 +0530

    Add UserModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your Name"],
+    maxLength: [4, "Name should be at least 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your Email"],
+    unique: true,
+    validators: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [8, "Password must be at least 8 characters long"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: 'user',
+  },
+  resetPasswordToken: String,
+  resetPasswordTokenExpire:Date,
+});
+
+module.exports = mongoose.model('User', userSchema);
