6ca20d50202ca7921d5031dade6ee344aaa52861
rutvij-fsd <rutvijpatel414@gmail.com>
2022-11-12 16:44:11 +0530

    Make Controller for user register

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncError = require("../middleware/catchAsyncError");
+const User = require('../models/userModel');
+
+//Register a User
+exports.registerUser = catchAsyncError( async (req, res, next) => {
+    const {name, email, password} = req.body;
+
+    const user = await User.create({
+        name,email,password,
+        avatar:{
+            public_id:"this-is-sample-public_id",
+            url: 'imageUrl',
+        }
+    });
+
+    res.status(201).json({
+        success: true,
+        user,
+    });
+})
